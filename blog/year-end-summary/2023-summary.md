c5b2cbffed83f2dba1bbb135ae37b88d13490bb9
yuluo-yx <yuluo08290126@gmail.com>
2025-02-03 22:05:28 +0800

    feat: add 2024 summary
    Signed-off-by: yuluo-yx <yuluo08290126@gmail.com>

--- blog/year-end-summary/2023-summary.md
+++ blog/year-end-summary/2023-summary.md
@@ -5,6 +5,7 @@ date: 2024-02-03 21:39:54
 authors: yuluo
 tags: ['年终总结', 2023]
 keywords: ['随笔', '年终总结']
+image: /img/summary/2023-summary.jpg
 ---
 
 <!-- truncate -->
--- data/friends.tsx
+++ data/friends.tsx
@@ -1,5 +1,10 @@
 export const Friends: Friend[] = [
-  // 暂无
+  {
+    title: 'pil0txia',
+    description: '不会摄影的白帽子不是好机长！',
+    website: 'https://www.pil0txia.com/',
+    avatar: 'https://avatars.githubusercontent.com/u/41445332',
+  },
 ]
 
 export type Friend = {
