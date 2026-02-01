37d4ef6dd2a418aeefbda33e8d130d34ab73142d
yuluo-yx <yuluo-yx@users.noreply.github.com>
2026-02-01 12:33:29 +0000

    deploy: 7577dff1b35c66a12b64382f91228ffb3fcca9cc

---
+++ assets/2025-summary-DDupvZxP.js
@@ -0,0 +1,13 @@
+const n=`---
+slug: 2025-year-summary
+title: 2025 年终总结
+date: 2026-02-03 20:12:00
+authors: yuluo
+tags: [Blog]
+keywords: [Blog]
+---
+
+<!-- truncate -->
+
+先占坑～
+`;export{n as default};
